0c7c5d7e495ea00eb977e707a7b6ebe735e97049
bell24216 <94054895+bell24216@users.noreply.github.com>
2026-03-10 15:44:25 +0700

    fix

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import json 
-import lazop
-from google.cloud import storage
-import os
 import json
+import lazop
+import os
 import time
-import hmac
-import hashlib
-import requests
 import logging
 from google.cloud import storage
 
@@ -18,34 +13,33 @@ def re_token(request):
     url = os.environ["URL"]
     appSecret = os.environ["APPSECRET"]
 
-    bucket_name = 'token_shopee_lazada_file'
-    file_name_old = 'token_lazada_new.json'
+    bucket_name = "token_shopee_lazada_file"
+    file_name = "token_lazada_new.json"
 
     client = storage.Client()
     bucket = client.bucket(bucket_name)
-    blob = bucket.blob(file_name_old)
+    blob = bucket.blob(file_name)
 
     data = json.loads(blob.download_as_string())
 
-    access_token = data['access_token']
-    refresh_token = data['refresh_token']
-    expires_at = data.get('expires_at')
+    refresh_token = data["refresh_token"]
+    expires_at = data.get("expires_at")
 
     now = int(time.time())
 
     # refresh ก่อนหมด 1 วัน
     if expires_at and now < (expires_at - 86400):
         logging.warning("Token ยังไม่ถึงเวลา refresh")
-        return
+        return "Token still valid"
 
     logging.warning("Token ใกล้หมดอายุ → Refresh token")
 
-    lazada_client = lazop.LazopClient(url, appkey ,appSecret)
+    lazada_client = lazop.LazopClient(url, appkey, appSecret)
 
-    request = lazop.LazopRequest('/auth/token/refresh')
-    request.add_api_param('refresh_token', refresh_token)
+    req = lazop.LazopRequest("/auth/token/refresh")
+    req.add_api_param("refresh_token", refresh_token)
 
-    response_new = lazada_client.execute(request)
+    response_new = lazada_client.execute(req)
     content = response_new.body
 
     access_token_new = content.get("access_token")
@@ -54,7 +48,7 @@ def re_token(request):
 
     if not access_token_new or not refresh_token_new:
         logging.error(content)
-        return
+        return "Refresh failed"
 
     expires_at_new = int(time.time()) + expires_in_new
 
@@ -68,3 +62,5 @@ def re_token(request):
     blob.upload_from_string(json.dumps(data_new))
 
     logging.warning("Token refreshed และบันทึกเรียบร้อย")
+
+    return "Token refreshed"
